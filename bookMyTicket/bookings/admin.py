76f9d49d34527ff431d0d659ee53ec4493e24c0c
ganesh-deshmukh <ganeshdeshmukh1612@gmail.com>
2021-04-24 21:15:14 +0530

    adding migrations py-dependencies

--- bookMyTicket/bookings/admin.py
+++ bookMyTicket/bookings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+admin.site.register(Customer)
+admin.site.register(City)
+admin.site.register(Theater)
+admin.site.register(Movie)
+admin.site.register(Shows)
+admin.site.register(Seats)
+admin.site.register(Booking)
