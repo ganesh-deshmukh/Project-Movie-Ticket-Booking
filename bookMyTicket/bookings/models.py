51bdce5c7a7ccfe8c200d199c489a5335375d165
ganesh-deshmukh <ganeshdeshmukh1612@gmail.com>
2021-04-24 13:12:23 +0530

    basic structure of class is created.

--- bookMyTicket/bookings/models.py
+++ bookMyTicket/bookings/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
-# Create your models here.
+# there might be two cities with same names, so we will assign each city a unique PIN-Code later.
+
+
+class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+	name = models.CharField(max_length=24, null=True)
+	phone = models.CharField(max_length=24, null=True)
+	email = models.CharField(max_length=24, null=True)
+	date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+	def __str__(self):
+		return self.name
+
+
+class City(models.Model):
+	name = models.CharField(max_length=24, null=True)
+    movie = models.ManyToManyField(Movie)
+    theater = models.ManyToManyField(Theater)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+	name = models.CharField(max_length=24, null=True)
+
+	def __str__(self):
+		return self.name
+
+
+
+class Theater(models.Model):
+	name = models.CharField(max_length=24, null=True)
+
+	def __str__(self):
+		return self.name
+
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=24, null=True)
+    
+    def __str__(self):
+        return self.name
+
+
+ 
