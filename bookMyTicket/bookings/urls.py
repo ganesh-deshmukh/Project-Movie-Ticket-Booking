a3443c434ad7958e1bd8966c47117f45aea75f54
ganesh-deshmukh <ganeshdeshmukh1612@gmail.com>
2021-04-25 15:17:48 +0530

    added views, routes for customers and admin.

--- bookMyTicket/bookings/urls.py
+++ bookMyTicket/bookings/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
 from . import views
+# from . import views
 
 urlpatterns = [
-    path('', views.home),
+    # Admin Routes
+    path('admin/home', views.Admin_Home),
+    path('admin/add/city', views.Admin_Add_City),
+    path('admin/add/movie', views.Admin_Add_Movie),
+    path('admin/add/theater', views.Admin_Add_Theater),
+    path('admin/add/show_with_seats', views.Admin_Add_Show_With_Seats),
+
+    # Customer Routes
+    path('', views.Cust_Home_Book_Now),
+    path('customer/select/city', views.Cust_Select_City),
+    path('customer/select/movie', views.Cust_Select_Movie),
+    path('customer/select/theater', views.Cust_Select_Theater),
+    path('customer/select/show_with_seat', views.Cust_Select_Show_With_Seat),
+    path('customer/booking/payment', views.Cust_Booking_Payment),
+
 ]
--- bookMyTicket/bookings/views.py
+++ bookMyTicket/bookings/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Admin Views
 
-def home(request):
-    return HttpResponse("Home Page Exported from view")
+def Admin_Home(request):
+    return render(request, 'bookings/Admin_Home.html')
+
+
+def Admin_Add_City(request):
+    return render(request, 'bookings/Admin_Add_City.html')
+
+
+def Admin_Add_Movie(request):
+    return render(request, 'bookings/Admin_Add_Movie.html')
+
+
+def Admin_Add_Theater(request):
+    return render(request, 'bookings/Admin_Add_Theater.html')
+
+
+def Admin_Add_Show_With_Seats(request):
+    return render(request, 'bookings/Admin_Add_Show_With_Seats.html')
+
+
+# Customer Views
+
+def Cust_Home_Book_Now(request):
+    return render(request, 'bookings/Cust_Home_Book_Now.html')
+
+
+def Cust_Select_City(request):
+    return render(request, 'bookings/Cust_Select_City.html')
+
+
+def Cust_Select_Movie(request):
+    return render(request, 'bookings/Cust_Select_Movie.html')
+
+
+def Cust_Select_Theater(request):
+    return render(request, 'bookings/Cust_Select_Theater.html')
+
+
+def Cust_Select_Show_With_Seat(request):
+    return render(render, 'bookings/Cust_Select_Show_With_Seat.html')
+
+
+def Cust_Booking_Payment(request):
+    return render(request, 'bookings/Cust_Booking_Payment.html')
+
