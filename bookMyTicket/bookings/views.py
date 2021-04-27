512399a608b63003ac130289e9c49706ca016062
ganesh-deshmukh <ganeshdeshmukh1612@gmail.com>
2021-04-27 20:17:29 +0530

    Added GIFs and steps for markdown file

--- bookMyTicket/bookings/views.py
+++ bookMyTicket/bookings/views.py
@@ -27,10 +27,8 @@ def LoginPage(request):
         # if user:
         # login(request, password)
         # redirect('home')
-        try:
-            user = User.objects.get(email=username)
-        except:
-            user = User.objects.get(username=username)
+        user = User.objects.get(email=username)
+            # user = User.objects.get(username=username)
 
         if user and (check_password(password, user.password) and user.is_active):
             login(request, user)
