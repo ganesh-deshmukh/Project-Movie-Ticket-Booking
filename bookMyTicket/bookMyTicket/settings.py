ba2b05a097486bc300cea86112a47c6f6b87cb06
ganesh-deshmukh <ganeshdeshmukh1612@gmail.com>
2021-04-28 13:39:02 +0530

    added dj_database_url, for postgres herko config

--- bookMyTicket/bookMyTicket/settings.py
+++ bookMyTicket/bookMyTicket/settings.py
@@ -119,9 +119,9 @@ DATABASES = {
         'PORT': ''
     }
 }
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
